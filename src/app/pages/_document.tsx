620505b94e615509b00f151b1f2455ba9deb5d15
luiscam7 <lcamargo2007@gmail.com>
2023-09-10 14:31:30 -0700

    update document.tsx

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -5,17 +5,13 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* Meta tags */}
-                    <meta name="theme-color" content="#042940" />
-
-                    {/* Manifest */}
                     <link rel="manifest" href="public/manifest.json" />
-
-                    {/* Apple touch icon */}
                     <link rel="apple-touch-icon" href="./images/logo/wave.png" />
-
-                    {/* Google font */}
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=optional" />
+                    <meta name="theme-color" content="#042940" />
+                    <meta
+                        name='viewport'
+                        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+                    />
                 </Head>
                 <body>
                     <Main />
