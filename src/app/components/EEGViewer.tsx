f4d0a8cc9d8c8cc2b487fc9e2ff087b93861504f
luiscam7 <lcamargo2007@gmail.com>
2023-09-09 22:54:59 -0700

    eeg-viewer

---
+++ src/app/components/EEGViewer.tsx
@@ -0,0 +1,71 @@
+// components/EEGViewer.tsx
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import '../styles/EEGViewer.css';
+
+const EEGViewer: React.FC = () => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  // Mock EEG data
+  const channels = 19;
+  const channelLabels = ["Fz", "Cz", "Pz", "Fp1", "Fp2", "F7", "F3", "F4", "F8", "T7", "C3", "C4", "T8", "P7", "P3", "P4", "P8", "O1", "O2"];
+  const mockEEGData = Array.from({ length: channels }, () => ({
+    values: Array.from({ length: 5000 }, () => Math.random() * 10 - 5),
+  }));
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    const width = +svg.attr('width'); 
+    const height = +svg.attr('height'); 
+    const margin = { top: 5, right: 0, bottom: 5, left: 50 };
+    const channelHeight = (height - margin.top - margin.bottom - (channels - 1) * margin.top) / channels;
+
+    function drawEEGData() {
+      // Remove existing paths
+      svg.selectAll("path").remove();
+
+    // Remove previous paths and text
+    svg.selectAll("*").remove();
+
+    mockEEGData.forEach((channel, i) => {
+      const y = (channelHeight + margin.top) * i + margin.top;
+
+      const xScale = d3.scaleLinear().domain([0, 1000]).range([margin.left, width - margin.right]);
+      const yScale = d3
+        .scaleLinear()
+        .domain([-5, 5])
+        .range([y + channelHeight, y]);
+
+      const lineGenerator = d3
+        .line<number>()
+        .x((d, index) => xScale(index))
+        .y(d => yScale(d));
+    
+      // Add EEG path
+      svg
+        .append('path')
+        .datum(channel.values)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 1.5)
+        .attr('d', lineGenerator);
+
+    // Add channel label
+    svg.append('text')
+        .attr('x', margin.left - 10) 
+        .attr('y', y + channelHeight / 2)
+        .attr('dy', '.35em')
+        .attr('text-anchor', 'end')
+        .text(channelLabels[i]);
+    });
+  }
+    drawEEGData();
+
+  }, [mockEEGData]);
+
+  return <svg ref={svgRef} width="1050" height="800"></svg>;
+};
+
+export default EEGViewer;
---
+++ src/app/components/Toolbar.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Image from 'next/image';
+import '../styles/Toolbar.css'; 
+
+interface BrainWaveToolbarProps {
+  userName: string;
+  userImage: string;
+  logoPath: string;
+}
+
+const BrainWaveToolbar: React.FC<BrainWaveToolbarProps> = ({ userName, userImage, logoPath }) => {
+  return (
+    <div className="toolbar-container px-4"> {/* Added 'px-4' for horizontal padding */}
+      <div className="flex justify-between items-center">
+          {/* Logo and title */}
+          <div className="flex items-center space-x-2">
+            <div className="relative w-20 h-12 shadow-md rounded-md">
+              <Image src={logoPath} alt="BrainWave logo" layout="fill" objectFit="cover" className="rounded-md" />
+            </div>
+            <h1 className="toolbar-title">BrainWave</h1>
+          </div>
+
+          {/* Toolbar Items */}
+          <div className="flex items-center space-x-4">
+            {['Import EEG', 'Analyze', 'Save', 'Settings'].map(action => (
+              <button 
+                key={action}
+                className="toolbar-button px-3 py-1"
+              >
+                {action}
+              </button>
+            ))}
+          </div>
+
+          {/* User Profile */}
+          <div className="flex items-center space-x-2">
+            <div className="relative w-8 h-8 shadow-md rounded-full">
+              <Image src={userImage} alt="User Profile" layout="fill" objectFit="cover" className="rounded-full" />
+            </div>
+            <span className="toolbar-title">{userName}</span>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default BrainWaveToolbar;
