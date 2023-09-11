23df4dae941c1f395ba839868cad9089064e0296
luiscam7 <lcamargo2007@gmail.com>
2023-09-11 11:12:44 -0700

    update toolbar

--- src/app/components/Toolbar.tsx
+++ src/app/components/Toolbar.tsx
@@ -12,18 +12,18 @@ interface BrainWaveToolbarProps {
 
 const BrainWaveToolbar: React.FC<BrainWaveToolbarProps> = ({ userName, userImage, logoPath }) => {
   return (
-    <div className={`toolbar-container px-4 shadow-lg dark:shadow-none dark:ring-1 dark:ring-gray-500`}>
-      {/* Logo Section */}
+    <div className={`toolbar-container flex justify-between items-center px-4 shadow-lg dark:shadow-none dark:ring-1 dark:ring-gray-500 w-full`}>
+      
       {/* Logo and title */}
-      <div className="relative w-8 h-8 shadow-md rounded-full">
-        <Image src={logoPath} alt="BrainWave logo" layout="fill" objectFit="cover" className="rounded-full" />
-      </div>
-      <div className="flex-1">
+      <div className="flex items-center gap-2">
+        <div className="relative w-8 h-8 shadow-md rounded-full">
+          <Image src={logoPath} alt="BrainWave logo" layout="fill" objectFit="cover" className="rounded-full" />
+        </div>
         <a className="btn btn-ghost normal-case text-xl">BrainWave</a>
       </div>
 
       {/* Toolbar Items */}
-      <div className="flex-none gap-2">
+      <div className="flex gap-2">
         {['Import EEG', 'Analyze', 'Settings'].map(action => (
           <button 
             key={action}
@@ -49,6 +49,7 @@ const BrainWaveToolbar: React.FC<BrainWaveToolbarProps> = ({ userName, userImage
           </ul>
         </div>
       </div>
+      
     </div>
   );
 };
