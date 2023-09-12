e9e690f72babcd3eee0dc6770d773bd3f56f4558
luiscam7 <lcamargo2007@gmail.com>
2023-09-12 15:34:13 -0700

    full screen PWA

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -8,6 +8,8 @@ class MyDocument extends Document {
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="apple-touch-icon" href="/wave.png" />
                     <meta name="theme-color" content="#042940" />
+                    <meta name="apple-mobile-web-app-capable" content="yes"/>
+                    <meta name="apple-mobile-web-app-status-bar-style" content="#042940"/>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=optional" />
                 </Head>
                 <body>
