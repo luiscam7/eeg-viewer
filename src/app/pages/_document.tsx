8862b03b39cf367ba19056935bbd658943f41ef0
luiscam7 <lcamargo2007@gmail.com>
2023-09-12 16:00:15 -0700

    add viewport in document

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
                     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
                     <meta name="apple-mobile-web-app-title" content="Cerebro"/>
                     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=optional" />
                 </Head>
                 <body>
