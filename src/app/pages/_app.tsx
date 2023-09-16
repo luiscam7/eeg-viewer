1c8a9cdb6826a7649edd70b6473e9b522ccbdeca
luiscam7 <lcamargo2007@gmail.com>
2023-09-16 16:38:02 -0700

    update display

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -7,10 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-      <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
         <meta name="application-name" content="Cerebro" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
